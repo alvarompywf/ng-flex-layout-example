d6b0859b5b0fa6e752b429102bbeb194e21231e2
Álvaro Mena Parra <alvaromepa@gmail.com>
2022-03-27 14:47:24 +0200

    practicando fxLayout de angular pasando de mobile a desktop y viceversa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnoComponent } from './uno/uno.component';
+import { DosComponent } from './dos/dos.component';
+import { TresComponent } from './tres/tres.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnoComponent,
+    DosComponent,
+    TresComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
